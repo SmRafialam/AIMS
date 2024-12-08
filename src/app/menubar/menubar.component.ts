5e9eaf3606d0ff0ce02eaae096294f7028d30300
S. M. Rafi <smrafialam@gmail.com>
2024-12-08 16:59:22 +0600

    completed sidebar design

--- src/app/menubar/menubar.component.ts
+++ src/app/menubar/menubar.component.ts
@@ -4,14 +4,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-menubar',
   standalone: true,
-  imports: [MatMenuModule,MatIconModule,MatToolbarModule,MatAutocompleteModule,MatButtonModule],
+  imports: [MatMenuModule,MatIconModule,MatToolbarModule,MatAutocompleteModule,MatButtonModule,MatSidenavModule, 
+    MatListModule,MatDividerModule ],
   templateUrl: './menubar.component.html',
   styleUrl: './menubar.component.css'
 })
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [ 
+      MatSidenavModule, 
+      MatListModule,    
+      MatIconModule,    
+      MatToolbarModule, 
+      MatButtonModule,  
+      MatDividerModule, 
+      MatExpansionModule
+    ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
